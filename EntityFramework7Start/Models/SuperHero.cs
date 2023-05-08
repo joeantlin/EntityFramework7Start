8f292e40c22c1715e699730aa0c787899ca7c710
Joe Linares <bwajal@gmail.com>
2023-05-08 15:27:31 -0700

    test

--- EntityFramework7Start/Models/SuperHero.cs
+++ EntityFramework7Start/Models/SuperHero.cs
@@ -6,7 +6,7 @@ namespace EntityFrameWork7Start
     public class SuperHero
 	{
         public int Id { get; set; }
-		public string Name { get; set; }
+		public string Name { get; set; } = string.Empty;
 		public string FirstName { get; set; } = string.Empty;
 		public string LastName { get; set; } = string.Empty;
 		public string Place { get; set; } = string.Empty;
