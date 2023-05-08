e16a288c2d4a8f2d39b0a1bba1e720a7aa10c1be
Joe Linares <bwajal@gmail.com>
2023-05-08 16:29:40 -0700

    we

--- EntityFramework7Start/Models/SuperHero.cs
+++ EntityFramework7Start/Models/SuperHero.cs
@@ -8,7 +8,7 @@ namespace EntityFrameWork7Start
         public int Id { get; set; }
 		public string Name { get; set; } = string.Empty;
 		public string FirstName { get; set; } = string.Empty;
-		public string LastName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;  
 		public string Place { get; set; } = string.Empty;
 	}
 }
